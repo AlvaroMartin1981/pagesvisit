1b0047ba75c7e8c7a8ffe951a8132be4b3f6532e
titopope@hotmail.com <titopope@hotmail.com>
2023-11-21 22:07:48 +0100

    primeros pasos

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*if(localStorage.pagecount){localStorage.pagecount=Number(localStorage.pagecount)+1;} else{localStorage.pagecount=1;}
+document.write("visitastes mi sitio"+localStorage.pagecount+"veces");
+section class="container"
+div class="content"
+button id="btnReestablecer"
+span id="contadorVisitas"
+*/
