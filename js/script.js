c6f3b99f4aa60301470ba184d6f31321ff3a0b16
titopope@hotmail.com <titopope@hotmail.com>
2023-11-23 16:11:55 +0100

    para entregar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-/*if(localStorage.pagecount){localStorage.pagecount=Number(localStorage.pagecount)+1;} else{localStorage.pagecount=1;}
-document.write("visitastes mi sitio"+localStorage.pagecount+"veces");
-section class="container"
-div class="content"
-button id="btnReestablecer"
-span id="contadorVisitas"
-*/
+const contadorVisitas= document.getElementById('contadorVisitas')
+const btnReestablecer= document.getElementById('btnReestablecer')
+
+if(localStorage.pagecount){
+    localStorage.pagecount=Number(localStorage.pagecount)+1;} 
+else{localStorage.pagecount=0;}
+
+contadorVisitas.innerHTML=localStorage.pagecount;
+btnReestablecer.addEventListener('click',function(){
+    localStorage.pagecount=0;
+console.log(contadorVisitas) 
+})
+contadorVisitas.innerHTML=localStorage.pagecount;
+
